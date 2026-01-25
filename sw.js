3faf409dcd331581b37797478a2ca6bbed77cb8d
Alex Voron <irreligious86@gmail.com>
2026-01-25 20:50:07 +0200

    Update offline logic

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'uav-report-v1.4.2';
+const CACHE_NAME = 'uav-report-v1';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
+  './manifest.json',
   './config.json',
-  './manifest.json'
+  './icon.png'
 ];
 
+// Установка: кешируем все файлы
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// Работа: берем файлы из кеша, если нет сети
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
